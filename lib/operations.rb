9f0694c84b11f69ad7da04ac60a95ab74285712a
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-18 13:38:58 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+  if speed < 40 > 60
+    "true"
+  else speed <= 50
+    "false"
 end
-
+unsafe?(speed)
 
 
 def not_safe?(speed)
-	
+	speed <40 > 60 ? "true" : "false"
 end
-	
+not_safe?(speed)
 
 
