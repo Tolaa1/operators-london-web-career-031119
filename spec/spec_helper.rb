ac59625c1af7423f3d2a6de27653d43f16c2f03c
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-18 14:07:11 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
  See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require_relative '../lib/operations.rb'
-#git@github.com:Tolaa1/operators-london-web-career-031119.git
 
 RSpec.configure do |config|
   # config here
